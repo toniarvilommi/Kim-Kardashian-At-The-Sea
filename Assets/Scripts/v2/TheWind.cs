aa9ba7ca5afddf57ac0870a3be52017ba9821d9e
Toni Arvilommi <toni.arvilommi@gmail.com>
2021-01-30 13:21:34 +0200

    New "wind" script

---
+++ Assets/Scripts/v2/TheWind.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheWind : MonoBehaviour
+{
+      public Rigidbody rb;
+      public int clickForce = 500;
+      private Plane plane = new Plane(Vector3.up, Vector3.zero);
+  
+      void Update () {
+            
+        if (Input.GetMouseButtonDown(0))
+         {
+             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+ 
+             float enter;
+             if (plane.Raycast(ray, out enter))
+             {
+                 var hitPoint = ray.GetPoint(enter);               
+                 var mouseDir = hitPoint - gameObject.transform.position;   
+                 mouseDir = mouseDir.normalized;    
+                 rb.AddForce(mouseDir * clickForce);
+             }
+         }
+      }
+}
+
