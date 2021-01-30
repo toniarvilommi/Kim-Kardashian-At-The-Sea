a1da51cc197ccdec097d95ed09ed4447b25b5563
Toni Arvilommi <toni.arvilommi@gmail.com>
2021-01-30 09:34:57 +0200

    Initial commit

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    private Plane plane;
+    public float moveSpeed = 3f;
+    public float turnSpeed = 3f;
+    public float angleLimit = 5f;
+    public float height = 1f;
+    public bool waitForRotation = true;
+    private void Start()
+    {
+        targetPosition = transform.position;
+        plane = new Plane(Vector3.up, new Vector3(0, height, 0));
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (plane.Raycast(ray, out float distance))
+            {
+                targetPosition = ray.GetPoint(distance);
+            }
+        }
+        if (targetPosition != transform.position)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(targetPosition - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+            if (!waitForRotation || Quaternion.Angle(targetRotation, transform.rotation) < angleLimit)
+            {
+                float distanceToDestination = Vector3.Distance(transform.position, targetPosition);
+                if (distanceToDestination > (Time.deltaTime * moveSpeed))
+                {
+                    transform.position += (targetPosition - transform.position).normalized * moveSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    transform.position = targetPosition;
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/v2/Waves.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waves : MonoBehaviour
+{
+    public int Dimension = 10;
+    public Octave[] Octaves;
+
+    protected MeshFilter MeshFilter;
+    protected Mesh Mesh;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Mesh = new Mesh();
+        this.Mesh.name = gameObject.name;
+
+        this.Mesh.vertices = GenerateVerts();
+        this.Mesh.triangles = GenerateTries();
+        this.Mesh.RecalculateBounds();
+
+        MeshFilter = gameObject.AddComponent<MeshFilter>();
+        MeshFilter.mesh = Mesh;
+    }
+
+    private Vector3[] GenerateVerts()
+    {
+        var verts = new Vector3[(Dimension + 1) * (Dimension + 1)];
+
+        //Equally distributed verts
+        for (int x = 0; x <= Dimension; x++)
+            for (int z = 0; z <= Dimension; z++)
+                verts[index(x, z)] = new Vector3(x, 0, z);
+
+        return verts;
+    }
+
+    private int index(int x, int z)
+    {
+        return x * (Dimension + 1) + z;
+    }
+
+
+    private int[] GenerateTries()
+    {
+        var tries = new int[Mesh.vertices.Length * 6];
+
+        for (int x = 0; x < Dimension; x++)
+        {
+            for (int z = 0; z < Dimension; z++)
+            {
+                tries[index(x, z) * 6 + 0] = index(x, z);
+                tries[index(x, z) * 6 + 1] = index(x + 1, z + 1);
+                tries[index(x, z) * 6 + 2] = index(x + 1, z);
+                tries[index(x, z) * 6 + 3] = index(x, z);
+                tries[index(x, z) * 6 + 4] = index(x, z + 1);
+                tries[index(x, z) * 6 + 5] = index(x + 1, z + 1);
+            }
+        }
+
+        return tries;
+    }
+
+   
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Take verts
+        var verts = Mesh.vertices;
+
+        //Manipulate verts
+        for (int x = 0; x <= Dimension; x++)
+        {
+            for (int z = 0; z <= Dimension; z++)
+            {
+                var y = 0f;
+                for (int o = 0; o < Octaves.Length; o++)
+                {
+                    if(Octaves[o].alternate)
+                    {
+                        //Perlin noise for dimension
+                        var perl = Mathf.PerlinNoise((x * Octaves[o].scale.x + Time.time * Octaves[o].speed.x) / Dimension, (z * Octaves[o].scale.y + Time.time * Octaves[o].speed.y) / Dimension) - 0.5f;
+                        //Cos for y
+                        y += Mathf.Cos(Octaves[o].speed.magnitude * Time.time) * Octaves[o].height;
+                    }
+                }
+
+                verts[index(x, z)] = new Vector3(x, y, z);
+            }
+        }
+        //Return verts
+        Mesh.vertices = verts;
+    }
+
+    [Serializable]
+    public struct Octave
+    {
+        public Vector2 speed;
+        public Vector2 scale;
+        public float height;
+        public bool alternate;
+    }
+
+}
