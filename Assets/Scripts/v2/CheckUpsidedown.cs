21dab4d12a267199508340762cbad234459ace44
Toni Arvilommi <toni.arvilommi@gmail.com>
2021-01-30 20:53:52 +0200

    Check upside down script

---
+++ Assets/Scripts/v2/CheckUpsidedown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckUpsidedown : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject _kim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Dot(transform.up, Vector3.down) > 0)
+        {
+            Debug.Log("Destroy joint");
+            Destroy(_kim.GetComponentInChildren<FixedJoint>());
+        }
+            
+    }
+}
